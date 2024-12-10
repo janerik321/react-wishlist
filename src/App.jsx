9760b670b85b8309a33a90fc3ec845638890d751
Jan Erik <janerik.kodehode@gmail.com>
2024-12-10 13:58:34 +0100

    Added naughty/nice cards. Some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 // import { useState } from 'react'
 
-// import "./App.css";
+import "./App.css";
+import styled from "styled-components";
 import Card from "./Card.jsx";
 import kidsWishlist from "./kidsWishlist.js";
+
+const Backdrop = styled.div`
+  width: 100%;
+  display: flex;
+  // flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  background-color: #114;
+  color: #fffd;
+`;
 
 function App() {
   return (
     <>
       <h1>Ønskelister</h1>
-      {kidsWishlist.map(({ name, age, wishlist, naughty }) => {
-        // if (!naughty) {
-
-        // }
-        return <>{Card(name, age, wishlist, naughty)}</>;
-      })}
+      <Backdrop>
+        {kidsWishlist.map(({ id, name, age, wishlist, naughty }) => {
+          // if (!naughty) {
+          // console.log(naughty);
+          // }
+          return <>{Card(id, name, age, wishlist, naughty)}</>;
+        })}
+      </Backdrop>
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,28 +1,127 @@
 import styled from "styled-components";
 
-const StyledCard = styled.div`
-  background: #cc4444;
-  color: #fffc;
-  padding: 0.5rem 1.5rem;
-  margin: 1rem 0;
+const NiceCard = styled.div`
+  width: 400px;
+  display: flex;
+  justify-content: center;
+  // gap: 4rem;
+  background: #c22;
+  color: #fffd;
+  padding: 2rem 2.5rem;
+  margin: 1rem;
+  border: 5px solid #fc1;
+  outline: 4px solid #fe1;
+  border-radius: 15px;
+  box-shadow: 0px 3px 4px inset #0005, 0px -1px 3px inset #0005,
+    0px 5px 10px -3px #0009;
   h4 {
-    margin-bottom: 0;
+    margin-bottom: 0.5rem;
+  }
+  ul {
+    padding-left: 1rem;
+  }
+
+  overflow: hidden;
+`;
+
+const Shine = styled.div`
+  // position: absolute;
+  // width: 400px;
+  // height: 30px;
+  // background-color: white;
+  // rotate: 45deg;
+  // left: 0px;
+  // // bottom: 0px;
+
+  // animation-name: shine;
+  // animation-duration: 5000ms;
+  // animation-iteration-count: infinite;
+  // // animation-delay: 2000ms;
+  // // animation-fill-mode: backwards;
+  // animation-timing-function: ease-in-out;
+
+  // @keyframes shine {
+  //   0% {
+  //     left: 0px;
+  //     bottom: 10px;
+  //   }
+  //   50% {
+  //     left: -10px;
+  //     bottom: 0px;
+  //     background-color: green;
+  //   }
+  // }
+  // // z-index: 1;
+`;
+
+const NaughtyCard = styled.div`
+  width: 400px;
+  display: flex;
+  justify-content: space-between;
+  // gap: 4rem;
+  background-color: #c22;
+  color: #fffd;
+  padding: 2rem 2.5rem;
+  margin: 1rem;
+  border: 5px solid #4a4;
+  border-radius: 15px;
+  box-shadow: 0px 3px 4px inset #0005, 0px -1px 3px inset #0005,
+    0px 5px 10px -3px #0009;
+  h4 {
+    margin-bottom: 0.5rem;
+  }
+  ul {
+    padding-left: 1rem;
   }
 `;
 
-export default function Card(name, age, wishlist, naughty) {
-  return (
-    <>
-      <StyledCard>
-        <h2>{name}</h2>
-        <h3>{age} år</h3>
-        <h4>Ønskeliste</h4>
-        <ul>
-          {wishlist.map((e) => {
-            return <li>{e}</li>;
-          })}
-        </ul>
-      </StyledCard>
-    </>
-  );
+const CardHalves = styled.div`
+  width: 50%;
+`;
+
+const NameAge = styled.div;
+
+export default function Card(id, name, age, wishlist, naughty) {
+  if (!naughty) {
+    return (
+      <>
+        <NiceCard>
+          <Shine />
+          <CardHalves>
+            <h2>{name}</h2>
+            <h3>{age} år</h3>
+          </CardHalves>
+          <CardHalves>
+            <h4>Ønskeliste</h4>
+            <ul>
+              {wishlist.map((e) => {
+                // console.log(`${id}-${e}`);
+                return <li key={`${id}-${e}`}>{e}</li>;
+              })}
+            </ul>
+          </CardHalves>
+        </NiceCard>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <NaughtyCard>
+          <CardHalves>
+            <h2>{name}</h2>
+            <h3>{age} år</h3>
+          </CardHalves>
+          <CardHalves>
+            <h4>Ønskeliste</h4>
+            <ul>
+              {wishlist.map((e) => {
+                // console.log(`${id}-${e}`);
+                return <li key={`${id}-${e}`}>{e}</li>;
+              })}
+            </ul>
+          </CardHalves>
+        </NaughtyCard>
+      </>
+    );
+  }
 }
